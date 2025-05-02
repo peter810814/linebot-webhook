be4f4f111dd876ce81c8ec4afcfde15a72a12b1a
peter810814 <nereus814@hotmail.com>
2025-05-02 18:19:54 +0800

    fix: correct env var names

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ import os
 app = Flask(__name__)
 
 # 用你自己的 token 與 secret 替換
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("K6ypXkjH40KHYgR2EsCBlcaMKSGcybyXh5IbvEm65b2/fM/Uxg8kTWRvXby5EyOMjv/7AZ+x4ikXbNQAPHCI9dYaOew1UkYxxu6yxO5PPuJX3Ci7K3Y8W0TX9Qno1RrkIlhD/l4mCCEXhrP/L29DbQdB04t89/1O/w1cDnyilFU")
-LINE_CHANNEL_SECRET = os.getenv("d097014786bbf942bfa1913723280b25")
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
 
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
